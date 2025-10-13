3649ed87a3bcb131c2c8769766959cae49ee5b70
Kim-SeonBeom <sktjsqja@naver.com>
2025-10-13 11:04:53 +0900

    Feat : first commit

---
+++ src/main/java/com/example/approval24/controller/testcontroller.java
@@ -0,0 +1,15 @@
+package com.example.approval24.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testcontroller {
+	
+	@GetMapping("/")
+	public String testmain() {
+		System.out.println("되냐?");
+		return "index";
+	}
+
+}
