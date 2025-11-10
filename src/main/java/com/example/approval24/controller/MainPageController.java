f08187559f56e802fd5cb9891ba6a3109db7469b
hongbagsa <20181425d@gmail.com>
2025-11-10 16:44:17 +0900

    메인페이지 수정

--- src/main/java/com/example/approval24/controller/MainPageController.java
+++ src/main/java/com/example/approval24/controller/MainPageController.java
@@ -1,17 +1,103 @@
 package com.example.approval24.controller;
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.example.approval24.domain.NoticeDTO;
+import com.example.approval24.service.AccountService;
+import com.example.approval24.service.ApprovalHistoryService;
+import com.example.approval24.service.NoticeService;
 
 
 
 @Controller
 public class MainPageController {
 	
+	
+	@Autowired
+	private NoticeService noticeService;
+	
+	@Autowired
+	private AccountService accountService;
+	
+	@Autowired
+	private ApprovalHistoryService accountHistoryService;
+	
+	
 	@GetMapping("/")
-	public String mainPage() {
+	public String mainPage(@SessionAttribute(name = "user", required = false) long accountId, Model model, HttpSession session) {
+		Long instId = accountService.findInstIdByAccountId(accountId);
+		// -------------인사---------------
+		// 내 기관 전체 계정
+		Map<String, Object> filter = new HashMap<>();
+		filter.put("instId", instId);
+		int myDetpCount= accountService.countAccountsByFilter(filter);
+		model.addAttribute("total", myDetpCount);
+		// 기관내 활성화 계정
+		filter.put("accountStatusCd", "B002");
+		filter.put("instId", instId);
+		int myDeptEnable = accountService.countAccountsByFilter(filter);
+		model.addAttribute("enableAccount", myDeptEnable);
+		
+		// 기관내 신청 대기
+		filter.remove("accountStatus");
+		filter.put("accountStatusCd", "B001");
+		int myDeptWaiting = accountService.countAccountsByFilter(filter);
+		model.addAttribute("waitingAccount", myDeptWaiting);
+		
+		// 기관내 정지(비번잠김)
+		filter.remove("accountStatus");
+		filter.put("accountStatusCd", "B005");
+		int myDeptLock = accountService.countAccountsByFilter(filter);
+		model.addAttribute("lockAccount", myDeptLock);
+		//-------------인사 끝--------------
+		
+		// -------------일반---------------
+		 Long userId = (Long) session.getAttribute("user");
+		Map<String, Object> filters = new HashMap<>();
+		
+		// 내 전체 민원
+		filters.put("accountId", userId);
+		int myTotalApproval = accountHistoryService.countMyAccountsByFilter(filters);
+		model.addAttribute("totalApproval", myTotalApproval);
+		
+		// 결재 대기중인 내 민원(History 상태 결재)
+		filters.put("approvalStatusCd", "E001");
+		int myWaitingApproval = accountHistoryService.countMyAccountsByFilter(filters);
+		model.addAttribute("waitingApproval", myWaitingApproval);
+		
+		
+		// 승인한 결재
+		filters.remove("approvalStatusCd");
+		// 승인
+		filters.put("approvalStatusCd", "E002");
+		int myRefuseApproval = accountHistoryService.countMyAccountsByFilter(filters);
+		model.addAttribute("refuseApproval", myRefuseApproval);
+		
+		// 반려한 결재
+		filters.remove("approvalStatusCd");
+		filters.put("approvalStatusCd", "E003");
+		int myapprovalsInTransit = accountHistoryService.countMyAccountsByFilter(filters);
+		model.addAttribute("approvalInTransit", myapprovalsInTransit);
+		// -------------일반 끝---------------
+		
+		// -------------공통----------------
+		// 공지사항
+		List<NoticeDTO> list = noticeService.noticeList();
+		model.addAttribute("noticeList", list);
+		// -------------공통 끝----------------
 		return "index";
 	}
+
 
 }
