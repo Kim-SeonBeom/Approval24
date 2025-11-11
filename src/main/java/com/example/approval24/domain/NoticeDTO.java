e1bbe3bb53296f883e366ceec936ff8533abd54b
hongbagsa <20181425d@gmail.com>
2025-11-11 20:10:54 +0900

    검색 공지란추가

--- src/main/java/com/example/approval24/domain/NoticeDTO.java
+++ src/main/java/com/example/approval24/domain/NoticeDTO.java
@@ -7,11 +7,10 @@ import lombok.Data;
 @Data
 
 public class NoticeDTO {
-	private long noticeId;
 	private String title;
-	private String content;
+	private String content;	
+	private long noticeId;
 	private String categoryCd;
-	private String categoryName;
 	private String userName;
 	private Date createDt;
 	private Date updateDt;
